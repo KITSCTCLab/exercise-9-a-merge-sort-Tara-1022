7df6a761773952108d4f3a6edf8d3a893d93dc6c
Tara URK21CS1022 <100012608+Tara-1022@users.noreply.github.com>
2022-10-08 21:20:23 +0530

    Initial Commit

--- Main.py
+++ Main.py
@@ -1,7 +1,31 @@
 from typing import List
 
+def merge(l1: [int], l2: [int]) -> [int]:
+  new = [0] * (len(l1) + len(l2))
+  i, j = 0, 0
+  k =  0
+  while i < len(l1) and j < len(l2):
+    if l1[i] < l2[j]:
+      new[k] = l1[i]
+      i += 1
+    else:
+      new[k] = l2[j]
+      j += 1
+    k += 1
+  while i < len(l1):
+    new[k] = l1[i]
+    i += 1
+  while j < len(l2):
+    new[k] = l2[j]
+    j += 1
+  return new
+
 def merge_sort(data) -> None:
-  # Write code here
+  if len(data) <= 1:
+    return
+  mid = len(data)//2
+  data = merge(merge_sort(data[:mid]), merge_sort(data[mid:]))
+  return
 
 
 # Do not change the following code
